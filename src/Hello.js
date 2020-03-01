df8e53b3a2772a5dfb9ccdbba154ef3a4457e677
Rodrigo Lopes <rodvlopes@gmail.com>
2020-02-29 22:59:41 -0300

    Hello world com webpack e babel.

---
+++ src/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Hello = () => 
+	<h1>Alo Mundo</h1>
+
+export default Hello
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require( 'path' );
+
+module.exports = {
+  entry: './src/App',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
+};
